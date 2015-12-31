41c9eea7c0f169dbb3f22bb7b509faa294a71eb0
Sal Ferrarello <sal@ironcodestudio.com>
2015-12-31 10:34:28 -0500

    Apply changes to oEmbed call rather than results
    Change from `oembed_result` hook to `oembed_fetch_url` hook
    
    Fixes #1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,7 @@ All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+- Apply changes to oEmbed call rather than results. Change from `oembed_result` hook to `oembed_fetch_url` hook
 
 ## [1.0.1] - 2015-12-31
 - Modify Example code to overwrite arguments rather than append. This is in
--- vimeo-oembed-modifications.php
+++ vimeo-oembed-modifications.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: Vimeo oEmbed Modifications
  * Plugin URI: https://github.com/salcode/vimeo-oembed-modifications
- * Description: Adds a filter "fe_vimeo_oembed_mods" that can be used to add URL parameters to the src attribute
+ * Description: Adds a filter "fe_vimeo_oembed_mods" that can be used to add URL arguments to the oEmbed request
  * Version: 1.0.1
  * Author: Sal Ferrarello
  * Author URI: http://salferrarello.com/
@@ -16,37 +16,18 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-add_filter('oembed_result', 'fe_vom_oembed_result', 10, 3);
+add_filter('oembed_fetch_url', 'fe_vom_oembed_fetch_url', 10, 3);
 
-function fe_vom_oembed_result( $data, $url, $args = array() ) {
+function fe_vom_oembed_fetch_url( $provider, $url, $args = array() ) {
 
 	// make no changes unless this is a vimeo video
-	if ( false === strpos( $data, 'player.vimeo.com' ) ) { return $data; }
+	if ( false === strpos( $provider, 'vimeo.com/api/oembed.json' ) ) { return $data; }
 
-	$str_to_add = apply_filters( 'fe_vimeo_oembed_mods', '' );;
+	$str_to_add = apply_filters( 'fe_vimeo_oembed_mods', '', $provider, $url, $args );
 
 	if ( '' === $str_to_add ) { return $data; }
 
-	// find where the iframe src attribute name begins
-	$src_name_begins = strpos( $data, 'src="' );
+	$provider .= $str_to_add;
 
-	if ( false === $src_name_begins ) {
-		if ( defined( WP_DEBUG ) && WP_DEBUG ) {
-			error_log( 'Unable to add oEmbed URL Parameters because the oEmbed data did not include "src="' );
-		}
-		return $data;
-	}
-
-	$src_start = $src_name_begins + strlen( 'src="' );
-	$src_end   = strpos( $data, '"', $src_start );
-
-	$pre  = substr( $data, 0,          $src_start );
-	$src  = substr( $data, $src_start, $src_end - $src_start );
-	$post = substr( $data, $src_end );
-
-	$src .= false === strpos( $src, '?' ) ? '?' : '&amp;';
-
-	$src .= $str_to_add;
-
-	return $pre . $src . $post;
+	return $provider;
 }
